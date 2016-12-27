73d40a8316c5b6777e1b411c8a13881aab0255d5
keatalay <kerem.atalay91@gmail.com>
2016-12-27 12:29:25 -0500

    updated to adhere to pythonic style conventions

--- doc_summ.py
+++ doc_summ.py
@@ -6,8 +6,8 @@ import operator
 import networkx as nx
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 
-#Assign variables to contain arguments passed
-#to script and read in target document.
+# Assign variables to contain arguments passed
+# to script and read in target document.
 if len(sys.argv) < 4:
     stem_type = '-p'
 else:
@@ -16,39 +16,37 @@ else:
 doc_path = sys.argv[1]
 sum_perc = float(sys.argv[2])
 doc = open(doc_path, 'r')
-doc = codecs.open(doc_path, encoding='utf-8', mode= 'r')
+doc = codecs.open(doc_path, encoding='utf-8', mode='r')
 
 text = doc.read()
-text = text.encode('ascii','ignore')
+text = text.encode('ascii', 'ignore')
 
-#Tokenize the document to be summarized
+# Tokenize the document to be summarized
 tok = PunktSentenceTokenizer()
 
 doc = ' '.join(text.strip().split('\n'))
 sentences = tok.tokenize(doc)
 
-#Pass tokenized document to tr_func.normalize
-#which generates a graph containing vertices
-#for each sentence in the document
+# Pass tokenized document to tr_func.normalize
+# which generates a graph containing vertices
+# for each sentence in the document
 graph = tr_func.normalize(sentences, stem_type)
 
-#Compute the similarty between each vertex,
-#use these similarities to create edges.
+# Compute the similarty between each vertex,
+# use these similarities to create edges.
 graph = tr_func.similarity(graph)
 
-#Use the nltk pagerank algorithm to generate
-#rankings.
+# Use the nltk pagerank algorithm to generate
+# rankings.
 rankings = nx.pagerank(graph, weight='wgt')
 
-#Sort rankings in descending order.
-sorted_rankings = sorted(rankings.items(), key=operator.itemgetter(1), reverse=True)
+# Sort rankings in descending order.
+sorted_rankings = sorted(
+    rankings.items(), key=operator.itemgetter(1), reverse=True)
 
-#Open a new file and write the document summary.
-out = open( doc_path.replace('.txt','') + '_Summary.txt', 'w')
+# Open a new file and write the document summary.
+out = open(doc_path.replace('.txt', '') + '_Summary.txt', 'w')
 sum_length = int(sum_perc * len(sentences))
 out.write('\t')
-for x in range(0,sum_length):
-    out.write( graph.node[sorted_rankings[x][0]]['raw'] + ' ')
-
-
-    
+for x in range(0, sum_length):
+    out.write(graph.node[sorted_rankings[x][0]]['raw'] + ' ')
